2e2bbc40127af674d5f1b5bf2b12be42b8bf5873
Jiaxen <jiaxen.lau@gmail.com>
2021-03-12 16:44:06 +0000

    Modified testing and removed superfluous

--- language_api/main.py
+++ language_api/main.py
@@ -1,17 +1,3 @@
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from datetime import datetime
 import logging
 import os
@@ -19,11 +5,9 @@ import os
 from flask import Flask, redirect, render_template, request
 
 from google.cloud import datastore
-# from google.cloud import storage
 from google.cloud import language_v1 as language
 
 
-# CLOUD_STORAGE_BUCKET = os.environ.get("CLOUD_STORAGE_BUCKET")
 
 
 app = Flask(__name__)
@@ -34,66 +18,24 @@ def homepage():
     # Create a Cloud Datastore client.
     datastore_client = datastore.Client()
 
-    # # Use the Cloud Datastore client to fetch information from Datastore about
-    # # each photo.
+    # # Use the Cloud Datastore client to fetch information from Datastore
+    # Query looks for all documents of the 'Sentences' kind, which is how we
+    # store them in upload_text()
     query = datastore_client.query(kind="Sentences")
     text_entities = list(query.fetch())
-    # text_entities = []
 
-    # # Return a Jinja2 HTML template and pass in image_entities as a parameter.
+    # # Return a Jinja2 HTML template and pass in text_entities as a parameter.
     return render_template("homepage.html", text_entities=text_entities)
 
 
 @app.route("/upload", methods=["GET", "POST"])
 def upload_text():
     text = request.form["text"]
-    # processed_text = text.upper()
-    # return processed_text
-    
-    # Create a Cloud Storage client.
-    # storage_client = storage.Client()
-
-    # Get the bucket that the file will be uploaded to.
-    # bucket = storage_client.get_bucket(CLOUD_STORAGE_BUCKET)
-
-    # Create a new blob and upload the file's content.
-    # blob = bucket.blob(photo.filename)
-    # blob.upload_from_string(photo.read(), content_type=photo.content_type)
-
-    # Make the blob publicly viewable.
-    # blob.make_public()
-
-    # Create a Cloud Vision client.
-    # vision_client = vision.ImageAnnotatorClient()
-
-
-
-    # Use the Cloud Vision client to detect a face for our image.
-    # source_uri = "gs://{}/{}".format(CLOUD_STORAGE_BUCKET, blob.name)
-    # image = vision.Image(source=vision.ImageSource(gcs_image_uri=source_uri))
-    # faces = vision_client.face_detection(image=image).face_annotations
-
-    # If a face is detected, save to Datastore the likelihood that the face
-    # displays 'joy,' as determined by Google's Machine Learning algorithm.
-    # if len(faces) > 0:
-    #     face = faces[0]
-
-        # Convert the likelihood string.
-    #     likelihoods = [
-    #         "Unknown",
-    #         "Very Unlikely",
-    #         "Unlikely",
-    #         "Possible",
-    #         "Likely",
-    #         "Very Likely",
-    #     ]
-    #     face_joy = likelihoods[face.joy_likelihood]
-    # else:
-    #     face_joy = "Unknown"
-
 
     # Analyse sentiment using Sentiment API call
     sentiment = analyze_text_sentiment(text)[0].get('sentiment score')
+
+    # Assign a label based on the score
     overall_sentiment = 'unknown'
     if sentiment > 0:
         overall_sentiment = 'positive'
@@ -108,23 +50,21 @@ def upload_text():
     # Fetch the current date / time.
     current_datetime = datetime.now()
 
-    # The kind for the new entity.
+    # The kind for the new entity. This is so all 'Sentences' can be queried.
     kind = "Sentences"
-
-    # The name/ID for the new entity.
-    # name = blob.name
 
     # Create the Cloud Datastore key for the new entity.
     key = datastore_client.key(kind, 'sample_task')
 
+    # Alternative to above, the following would store a history of all previous requests as no key
+    # identifier is specified, only a 'kind'. Datastore automatically provisions numeric ids.
+    # key = datastore_client.key(kind)
+
     # Construct the new entity using the key. Set dictionary values for entity
-    # keys blob_name, storage_public_url, timestamp, and joy.
     entity = datastore.Entity(key)
-    # entity["blob_name"] = blob.name
     entity["text"] = text
     entity["timestamp"] = current_datetime
     entity["sentiment"] = overall_sentiment
-    # entity["joy"] = face_joy
 
     # Save the new entity to Datastore.
     datastore_client.put(entity)
--- language_api/main_test.py
+++ language_api/main_test.py
@@ -1,17 +1,3 @@
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import uuid
 
 import backoff
@@ -22,9 +8,6 @@ import six
 
 import main
 
-TEST_PHOTO_URL = (
-    'https://upload.wikimedia.org/wikipedia/commons/5/5e/'
-    'John_F._Kennedy%2C_White_House_photo_portrait%2C_looking_up.jpg')
 
 
 @pytest.fixture
@@ -40,15 +23,13 @@ def test_index(app):
 
 
 def test_upload_photo(app):
-    test_photo_data = requests.get(TEST_PHOTO_URL).content
-    test_photo_filename = 'flex_and_vision_{}.jpg'.format(uuid.uuid4().hex)
 
     @backoff.on_exception(backoff.expo, GatewayTimeout, max_time=120)
     def run_sample():
         return app.post(
-            '/upload_photo',
+            '/upload_text',
             data={
-                'file': (six.BytesIO(test_photo_data), test_photo_filename)
+                'text': 'Test statement'
             }
         )
 
