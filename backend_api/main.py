8705db2dfa9c0adfafa574ffb37788eb79f6251b
Jiaxen <jiaxen.lau@gmail.com>
2021-09-09 12:26:20 +0100

    Added links to GCP docs and some doc strings

--- backend_api/main.py
+++ backend_api/main.py
@@ -10,14 +10,30 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
 
 """
 This Flask app shows some examples of the types of requests you could build.
-There is currently a GET request that will return all the data in GCP Datastore
-There is also a POST request that will analyse some given text then store the text and its sentiment in GCP Datastore
+There is currently a GET request that will return all the data in GCP Datastore.
+There is also a POST request that will analyse some given text then store the text and its sentiment in GCP Datastore.
+
+
+The sentiment analysis of the text is being done by Google's NLP API. 
+This API can be used to find the Sentiment, Entities, Entity-Sentiment, Syntax, and Content-classification of texts.
+Find more about this API here:
+https://cloud.google.com/natural-language/docs/basics
+For sample code for implementation, look here (click 'Python' above the code samples):
+https://cloud.google.com/natural-language/docs/how-to
+Note: The analyze_text_sentiment() method below simply copies the 'Sentiment' part of the above documentation.
+
+
+The database we are using is GCP Datastore (AKA Firestore in Datastore mode). 
+This is a simple NoSQL Document database offering by Google:
+https://cloud.google.com/datastore
+You can access the database through the GCP Cloud Console (find Datastore in the side-menu)
 
 
 Some ideas of things to build:
 - GET Request that returns a single entity based on its ID
 - POST Request that will take a list of text items and give it a sentiment then store it in GCP Datastore
 - DELETE Request to delete an entity from Datastore based on its ID
+
 
 We are using Flask: https://flask.palletsprojects.com/en/2.0.x/
 Flask RESTX is an extension of Flask that allows us to document the API with Swagger: https://flask-restx.readthedocs.io/en/latest/
@@ -33,8 +49,13 @@ parser.add_argument("text", type=str, help="Text", location="form")
 @api.route("/api/text")
 class Text(Resource):
     def get(self):
+        """
+        This GET request will return all the texts and sentiments that have been POSTed previously.
+        """
         # Create a Cloud Datastore client.
         datastore_client = datastore.Client()
+
+        # Get the datastore 'kind' which are 'Sentences'
         query = datastore_client.query(kind="Sentences")
         text_entities = list(query.fetch())
 
@@ -51,11 +72,15 @@ class Text(Resource):
 
     @api.expect(parser)
     def post(self):
+        """
+        This POST request will return the sentiment analysis of the first sentence of a given piece of text.
+        """
         datastore_client = datastore.Client()
 
         args = parser.parse_args()
         text = args["text"]
 
+        # Get the sentiment score of the first sentence of the analysis (that's the [0] part)
         sentiment = analyze_text_sentiment(text)[0].get("sentiment score")
 
         # Assign a label based on the score
@@ -72,9 +97,11 @@ class Text(Resource):
         # The kind for the new entity. This is so all 'Sentences' can be queried.
         kind = "Sentences"
 
-        # If a key is not specified then datastore will automatically generate one
-        # key = datastore_client.key(kind, 'sample_task')
+        # Create a key to store into datastore
         key = datastore_client.key(kind)
+        # If a key id is not specified then datastore will automatically generate one. For example, if we had:
+        # key = datastore_client.key(kind, 'sample_task')
+        # instead of the above, then 'sample_task' would be the key id used.
 
         # Construct the new entity using the key. Set dictionary values for entity
         entity = datastore.Entity(key)
@@ -109,17 +136,25 @@ def server_error(e):
 
 
 def analyze_text_sentiment(text):
+    """
+    This is modified from the Google NLP API documentation found here:
+    https://cloud.google.com/natural-language/docs/analyzing-sentiment
+    It makes a call to the Google NLP API to retrieve sentiment analysis.
+    """
     client = language.LanguageServiceClient()
     document = language.Document(content=text, type_=language.Document.Type.PLAIN_TEXT)
 
     response = client.analyze_sentiment(document=document)
 
+    # Format the results as a dictionary
     sentiment = response.document_sentiment
     results = dict(
         text=text,
         score=f"{sentiment.score:.1%}",
         magnitude=f"{sentiment.magnitude:.1%}",
     )
+
+    # Print the results for observation
     for k, v in results.items():
         print(f"{k:10}: {v}")
 
